1c5cdea0529858bfa2dc892ba59cd22aa0da9b6f
Aliaksei Petukhou <petukhou_a@sam-solutions.net>
2020-01-27 10:30:12 +0300

    Add solution for step 5, several links

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,7 +9,12 @@ class ProductPage(BasePage):
         add_button.click()
         
     def should_name_be_same(self):
-        assert self.browser.find_element(*ProductPageLocators.NAMETOVARA1).text == self.browser.find_element(*ProductPageLocators.NAMETOVARA2).text, "The title is different"
+        
+        
+        #if self.browser.find_element(*ProductPageLocators.NAMETOVARA1).text != self.browser.find_element(*ProductPageLocators.NAMETOVARA2).text:
+            #curbr = self.browser.current_url
+            #print (self.browser.current_url)
+        assert self.browser.find_element(*ProductPageLocators.NAMETOVARA1).text == self.browser.find_element(*ProductPageLocators.NAMETOVARA2).text, f"The title is different on the page {self.browser.current_url}"
         
     def should_price_be_equal(self):
-        assert self.browser.find_element(*ProductPageLocators.PRICE1).text == self.browser.find_element(*ProductPageLocators.PRICE2).text, "The price is different"
+        assert self.browser.find_element(*ProductPageLocators.PRICE1).text == self.browser.find_element(*ProductPageLocators.PRICE2).text, f"The price is different on the page {self.browser.current_url}"
