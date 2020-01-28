7beedf478e3990a687b89d51628b49215683f982
Aliaksei Petukhou <petukhou_a@sam-solutions.net>
2020-01-28 09:36:18 +0300

    Changing conftest.py

--- conftest.py
+++ conftest.py
@@ -12,19 +12,24 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    language = request.config.getoption("language")
+    user_language = request.config.getoption("language")
     browser = None
-    options = Options()  
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(options=options)
+        options = Options()
+        options.add_argument("--no-sandbox")
+        options.add_experimental_option('prefs', {'intl.accept_languages' : user_language})
+        print("\nstart chrome browser for test...")
+        browser = webdriver.Chrome("chromedriver", options = options)
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        firefox_profile = webdriver.FirefoxProfile()
+        firefox_profile.set_preference("intl.accept_languages", user_language)
+        print("\nstart firefox browser for test...")
+        browser = webdriver.Firefox(firefox_profile = firefox_profile)
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError("--browser_name should be chrome or firefox")    
     yield browser
-    print("\nquit browser..")
+    print("\nquit browser...")
+    browser.quit()
+
     
 
